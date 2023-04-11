b71bfda724d342163f54f2a5702d93515b09d443
rickkky <rickkky@foxmail.com>
2023-04-11 17:12:15 +0000

    fix: update random rectangle

--- demo/random-rectangle/src/main.ts
+++ demo/random-rectangle/src/main.ts
@@ -27,13 +27,12 @@ gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
 
 const colorLocation = gl.getUniformLocation(program, 'u_color');
+
 for (let i = 0; i < 50; i++) {
-  const x0 = (Math.random() * gl.canvas.width) / 2;
-  const y0 = (Math.random() * gl.canvas.height) / 2;
-  const width = (Math.random() * gl.canvas.width) / 2;
-  const height = (Math.random() * gl.canvas.height) / 2;
-  const x1 = x0 + width;
-  const y1 = y0 + height;
+  const x0 = Math.random() * gl.canvas.width;
+  const y0 = Math.random() * gl.canvas.height;
+  const x1 = Math.random() * gl.canvas.width;
+  const y1 = Math.random() * gl.canvas.height;
   gl.bufferData(
     gl.ARRAY_BUFFER,
     new Float32Array([x0, y0, x0, y1, x1, y0, x0, y1, x1, y1, x1, y0]),
