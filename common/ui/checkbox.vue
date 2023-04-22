79e7e93933ca14ec09f14e04e5d6e28a86e1cb80
rickkky <rickkky@foxmail.com>
2023-04-22 12:44:52 +0800

    feat(ui): checkbox

---
+++ common/ui/checkbox.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  label: string;
+  modelValue: boolean;
+}>();
+
+const emit = defineEmits<{
+  (e: 'update:modelValue', value: boolean): void;
+}>();
+</script>
+
+<template>
+  <div class="ui-checkbox">
+    <input
+      type="checkbox"
+      :checked="props.modelValue"
+      @change="
+        emit('update:modelValue', ($event.target! as HTMLInputElement).checked)
+      "
+    />
+    <label>{{ props.label }}</label>
+  </div>
+</template>
