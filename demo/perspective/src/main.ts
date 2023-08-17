ce87dfab9f618100e7501d89f6b82389e26f9b2f
rickkky <rickkky@foxmail.com>
2023-08-17 23:31:13 +0800

    feat: perspective demo

---
+++ demo/perspective/src/main.ts
@@ -0,0 +1,182 @@
+import { resizeCanavsToDisplaySize, createProgram } from '/common/helper';
+import fragmentShaderSource from './fragment.glsl';
+import vertexShaderSource from './vertex.glsl';
+import * as data from './data';
+import { statehub, state } from './state';
+
+const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+const gl = canvas.getContext('webgl2')!;
+
+const program = createProgram(gl, vertexShaderSource, fragmentShaderSource);
+
+const vao = gl.createVertexArray();
+
+const positionLocation = gl.getAttribLocation(program, 'a_position');
+const positionBuffer = gl.createBuffer();
+
+const colorLocation = gl.getAttribLocation(program, 'a_color');
+const colorBuffer = gl.createBuffer();
+
+const matrixLocation = gl.getUniformLocation(program, 'u_matrix');
+
+statehub.settle(render);
+
+function render() {
+  resizeCanavsToDisplaySize(gl.canvas as HTMLCanvasElement);
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+  gl.clearColor(0, 0, 0, 0);
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+  gl.useProgram(program);
+
+  gl.bindVertexArray(vao);
+
+  const positions = data.positions;
+  gl.enableVertexAttribArray(positionLocation);
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+  gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 0, 0);
+
+  const colors = data.colors;
+  gl.enableVertexAttribArray(colorLocation);
+  gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Uint8Array(colors), gl.STATIC_DRAW);
+  gl.vertexAttribPointer(colorLocation, 3, gl.UNSIGNED_BYTE, true, 0, 0);
+
+  const matrix = transform(
+    perspective(
+      state.fov * (Math.PI / 180),
+      gl.canvas.width / gl.canvas.height,
+      1,
+      2000,
+    ),
+    translation(state.tx, state.ty, state.tz),
+    translation(state.ox, state.oy, state.oz),
+    rotationZ(state.rz * (Math.PI / 180)),
+    rotationY(state.ry * (Math.PI / 180)),
+    rotationX(state.rx * (Math.PI / 180)),
+    scaling(state.sx, state.sy, state.sz),
+    translation(-state.ox, -state.oy, -state.oz),
+  );
+  gl.uniformMatrix4fv(matrixLocation, false, matrix);
+
+  if (state.enableCullFace) {
+    gl.enable(gl.CULL_FACE);
+  } else {
+    gl.disable(gl.CULL_FACE);
+  }
+  if (state.enableDepthTest) {
+    gl.enable(gl.DEPTH_TEST);
+  } else {
+    gl.disable(gl.DEPTH_TEST);
+  }
+
+  gl.drawArrays(gl.TRIANGLES, 0, positions.length / 2);
+}
+
+function transform(...ms: number[][]) {
+  let m = ms[0];
+  for (let i = 1; i < ms.length; i++) {
+    m = multiply(m, ms[i]);
+  }
+  return m;
+}
+
+function multiply(a: number[], b: number[]) {
+  const c = [];
+  for (let i = 0; i < 4; i++) {
+    c[i * 4 + 0] =
+      a[0 * 4 + 0] * b[i * 4 + 0] +
+      a[1 * 4 + 0] * b[i * 4 + 1] +
+      a[2 * 4 + 0] * b[i * 4 + 2] +
+      a[3 * 4 + 0] * b[i * 4 + 3];
+    c[i * 4 + 1] =
+      a[0 * 4 + 1] * b[i * 4 + 0] +
+      a[1 * 4 + 1] * b[i * 4 + 1] +
+      a[2 * 4 + 1] * b[i * 4 + 2] +
+      a[3 * 4 + 1] * b[i * 4 + 3];
+    c[i * 4 + 2] =
+      a[0 * 4 + 2] * b[i * 4 + 0] +
+      a[1 * 4 + 2] * b[i * 4 + 1] +
+      a[2 * 4 + 2] * b[i * 4 + 2] +
+      a[3 * 4 + 2] * b[i * 4 + 3];
+    c[i * 4 + 3] =
+      a[0 * 4 + 3] * b[i * 4 + 0] +
+      a[1 * 4 + 3] * b[i * 4 + 1] +
+      a[2 * 4 + 3] * b[i * 4 + 2] +
+      a[3 * 4 + 3] * b[i * 4 + 3];
+  }
+  return c;
+}
+
+function perspective(fov: number, aspect: number, near: number, far: number) {
+  const f = Math.tan(0.5 * Math.PI - 0.5 * fov);
+  const rangeInv = 1 / (near - far);
+  // prettier-ignore
+  return [
+    f / aspect, 0, 0,                         0,
+    0,          f, 0,                         0,
+    0,          0, (near + far) * rangeInv,  -1,
+    0,          0, 2 * near * far * rangeInv, 0,
+  ]
+}
+
+function translation(tx: number, ty: number, tz: number) {
+  // prettier-ignore
+  return [
+    1,  0,  0,  0,
+    0,  1,  0,  0,
+    0,  0,  1,  0,
+    tx, ty, tz, 1,
+  ];
+}
+
+// Rotate direction: facing negative x axis with counter-clockwise.
+function rotationX(rad: number) {
+  const c = Math.cos(rad);
+  const s = Math.sin(rad);
+  // prettier-ignore
+  return [
+    1,  0, 0, 0,
+    0,  c, s, 0,
+    0, -s, c, 0,
+    0,  0, 0, 1,
+  ];
+}
+
+// Rotate direction: facing negative y axis with counter-clockwise.
+function rotationY(rad: number) {
+  const c = Math.cos(rad);
+  const s = Math.sin(rad);
+  // prettier-ignore
+  return [
+    c, 0, -s, 0,
+    0, 1,  0, 0,
+    s, 0,  c, 0,
+    0, 0,  0, 1,
+  ];
+}
+
+// Rotate direction: facing negative z axis with counter-clockwise.
+function rotationZ(rad: number) {
+  const c = Math.cos(rad);
+  const s = Math.sin(rad);
+  // prettier-ignore
+  return [
+     c, s, 0, 0,
+    -s, c, 0, 0,
+     0, 0, 1, 0,
+     0, 0, 0, 1,
+  ];
+}
+
+function scaling(sx: number, sy: number, sz: number) {
+  // prettier-ignore
+  return [
+    sx, 0,  0,  0,
+    0,  sy, 0,  0,
+    0,  0,  sz, 0,
+    0,  0,  0,  1,
+  ];
+}
---
+++ demo/perspective/src/state.ts
@@ -0,0 +1,185 @@
+import { createStatehub } from '/common/statehub';
+
+export const statehub = await createStatehub();
+
+export const ox = statehub.setup({
+  label: 'Origin X',
+  type: 'slider',
+  props: {
+    min: 0,
+    max: 100,
+  },
+  default: 0,
+});
+export const oy = statehub.setup({
+  label: 'Origin Y',
+  type: 'slider',
+  props: {
+    min: -20,
+    max: 0,
+  },
+  default: 0,
+});
+export const oz = statehub.setup({
+  label: 'Origin Z',
+  type: 'slider',
+  props: {
+    min: 0,
+    max: 20,
+  },
+  default: 10,
+});
+export const sx = statehub.setup({
+  label: 'Scale X',
+  type: 'slider',
+  props: {
+    min: -2,
+    max: 2,
+  },
+  default: 1,
+});
+export const sy = statehub.setup({
+  label: 'Scale Y',
+  type: 'slider',
+  props: {
+    min: -2,
+    max: 2,
+  },
+  default: 1,
+});
+export const sz = statehub.setup({
+  label: 'Scale Z',
+  type: 'slider',
+  props: {
+    min: -2,
+    max: 2,
+  },
+  default: 1,
+});
+export const rx = statehub.setup({
+  label: 'Rotate X ',
+  type: 'slider',
+  props: {
+    min: -360,
+    max: 360,
+  },
+  default: 45,
+});
+export const ry = statehub.setup({
+  label: 'Rotate Y',
+  type: 'slider',
+  props: {
+    min: -360,
+    max: 360,
+  },
+  default: 45,
+});
+export const rz = statehub.setup({
+  label: 'Rotate Z ',
+  type: 'slider',
+  props: {
+    min: -360,
+    max: 360,
+  },
+  default: -45,
+});
+export const tx = statehub.setup({
+  label: 'Translate X',
+  type: 'slider',
+  props: {
+    min: -400,
+    max: 400,
+  },
+  default: -150,
+});
+export const ty = statehub.setup({
+  label: 'Translate Y',
+  type: 'slider',
+  props: {
+    min: -400,
+    max: 400,
+  },
+  default: 0,
+});
+export const tz = statehub.setup({
+  label: 'Translate Z',
+  type: 'slider',
+  props: {
+    min: -400,
+    max: 400,
+  },
+  default: -360,
+});
+export const fov = statehub.setup({
+  label: 'Field of View',
+  type: 'slider',
+  props: {
+    min: 0,
+    max: 180,
+  },
+  default: 60,
+});
+export const enableCullFace = statehub.setup({
+  label: 'Enable Culling Face',
+  type: 'checkbox',
+  props: {
+    label: '',
+  },
+  default: true,
+});
+export const enableDepthTest = statehub.setup({
+  label: 'Enable Depth Test',
+  type: 'checkbox',
+  props: {
+    label: '',
+  },
+  default: true,
+});
+
+export const state = {
+  get ox() {
+    return ox.value;
+  },
+  get oy() {
+    return oy.value;
+  },
+  get oz() {
+    return oz.value;
+  },
+  get sx() {
+    return sx.value;
+  },
+  get sy() {
+    return sy.value;
+  },
+  get sz() {
+    return sz.value;
+  },
+  get rx() {
+    return rx.value;
+  },
+  get ry() {
+    return ry.value;
+  },
+  get rz() {
+    return rz.value;
+  },
+  get tx() {
+    return tx.value;
+  },
+  get ty() {
+    return ty.value;
+  },
+  get tz() {
+    return tz.value;
+  },
+  get fov() {
+    return fov.value;
+  },
+  get enableCullFace() {
+    return enableCullFace.value;
+  },
+  get enableDepthTest() {
+    return enableDepthTest.value;
+  },
+};
